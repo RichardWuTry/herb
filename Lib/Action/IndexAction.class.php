40121ac418abb47ad62bd63311347a8edb4a73a1
DBFocus <shadow_wu82@163.com>
2012-06-27 23:17:25 +0800

    add login module

--- Lib/Action/IndexAction.class.php
+++ Lib/Action/IndexAction.class.php
@@ -2,6 +2,22 @@
 // 本类由系统自动生成，仅供测试用途
 class IndexAction extends Action {
     public function index(){
-		
+		$this->display();
     }
+	
+	public function login(){
+		if (!$this->isPost()) {
+			redirect_to(__URL__.'/index');
+		}
+		
+		$staff_name = $_POST['login'];
+		$password = $_POST['password'];
+		
+		$Staff = M('Staff');
+		if ($Staff->where("staff_name = '$staff_name' and password = '$password'")->find()) {
+			$this->success();
+		} else {
+			$this->error('Incorrect name or password.');
+		}
+	}
 }
