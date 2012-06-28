bf4a685c1202741110c5448d9a26e10b555a7292
DBFocus <shadow_wu82@163.com>
2012-06-28 23:19:22 +0800

    not finish update sole_form.css

---
+++ Lib/Action/CustomerAction.class.php
@@ -0,0 +1,13 @@
+<?php
+// 本类由系统自动生成，仅供测试用途
+class CustomerAction extends Action {
+	public function add() {
+		$this->display();
+	}
+
+	public function search() {
+		$this->display();
+	}
+}
+
+?>
