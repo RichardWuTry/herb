3f081023f9e5c7f0077b670851a29d76752a3aa1
DBFocus <shadow_wu82@163.com>
2012-07-04 22:58:25 +0800

    Recipe add module

--- Lib/Action/RecipeAction.class.php
+++ Lib/Action/RecipeAction.class.php
@@ -7,19 +7,41 @@ class RecipeAction extends Action {
 		}
 	}
 	
-	public function list() {
+	
+	public function lists() {
 		if (isset($_GET['id'])) {
 			$customer_id = $_GET['id'];
-			
 		}
 	}
 	
 	public function add() {
-	
+		if (isset($_GET['cid'])) {
+			$customer_id = $_GET['cid'];
+			$Cust = M('Customer');
+			if ($customer = $Cust
+							->where("customer_id = $customer_id")
+							->find()) {
+				$this->assign('customer', $customer);
+				$this->assign('staff_id', $_SESSION['user_id']);
+				$this->assign('staff_name', $_SESSION['user_name']);
+				
+				if (isset($_GET['rid'])) {
+					$recipe_id = $_GET['rid'];
+					$Reci = M('Reci');
+					if ($recipe = $Reci
+								->where("recipe_id = $recipe_id")
+								->find()) {
+						$this->assign('recipe', $recipe);
+					}
+				}
+
+				$this->display();
+			}
+		}
 	}
 	
 	public function edit() {
-	
+		
 	}
 }
 ?>
