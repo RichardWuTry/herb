fb42f1386a09df5f9d0332a9be2a91e3110370c8
DBFocus <shadow_wu82@163.com>
2012-07-14 23:23:13 +0800

    update print function

--- Public/javascript/jqTools.js
+++ Public/javascript/jqTools.js
@@ -22,3 +22,8 @@ function showBox(msgBox, fColor, bgColor) {
 	}
 	box.show();
 }
+
+function nl2br(str, is_xhtml) {   
+	var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '\<br \/>' : '\<br>';    
+	return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1'+ breakTag +'$2');
+}
